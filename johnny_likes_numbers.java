c1c6b5111fa407027735874ae681ebfbe7b0666d
Nabil Sakran <sakran@mcmaster.ca>
2016-06-23 20:10:47 -0500

    Condensed original correct answer for Johnny likes numbers and resulted in correct answer again

--- johnny_likes_numbers.java
+++ johnny_likes_numbers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class answer2 {
+public class moreefficientanswer {
 
 	static Long result=0L;
 
@@ -17,46 +17,19 @@ public class answer2 {
 	}
 
 	public static void process(String[] input){
-		
+
 		Long n= Long.parseLong(input[0]);
 		Long k= Long.parseLong(input[1]);
-		Long i= k;
-		Long count=1L;
-		Long result=0L;
 
-		if(n>=1 && k<=Math.pow(10,9)){
-			if (k>n){
-				result=k;
-			}
+		Long result= ((n/k)+1)*k;
 
-			else if ((k*1000000)>n){
-				while (result<=n){	
-					result=i*count;
-					count++;
-				}
-			}
-			else{
-				result=n+1;
-				while(result>n){
-					if(result%k==0){
-						break;
-					}
-					else{
-						result++;
-					}
-				}
-			}
-		}
 		System.out.println(result);
-		
-
 	}
 
 	public static void main(String[] args){
 
 		String[] inputval=input();
 		process(inputval);
-
 
 	}
 }
